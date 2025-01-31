70e527506e5ed83e95da3da9394748802c79ed0e
Wienerbroed <144680695+Wienerbroed@users.noreply.github.com>
2025-01-31 10:34:12 +0100

    Added the variables file

---
+++ 01._Miscellanous/variables.js
@@ -0,0 +1,32 @@
+
+
+const me = {};
+
+me.name = "Martin";
+
+console.log(me)
+
+
+const hobbies = ["sleeping","coding"];
+
+hobbies.pop();
+
+hobbies.push("drawing", "painting")
+
+console.log(hobbies);
+
+
+const dancerName = "Computer";
+
+console.log("I can't run")
+console.log('I can "run!"')
+//String interpolation
+console.log(`I can't "run"`);
+
+
+const assignmentDescription = "...and the value is...";
+const value = 4;
+
+console.log(assignmentDescription, value)
+
+
